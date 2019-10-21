18009dab875c933e34a15e15f399db0e64769288
alexander-horne <55353662+alexander-horne@users.noreply.github.com>
2019-10-21 17:34:27 +0200

    add gossip id

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,8 +1,10 @@
 require 'gossip'
 
 class ApplicationController < Sinatra::Base
+
+
     get '/' do
-        erb :index
+        erb :index, locals: {gossips: Gossip.all}
     end
 
     get '/gossips/new/' do
@@ -10,15 +12,12 @@ class ApplicationController < Sinatra::Base
     end
     
     post '/gossips/new/' do
-        Gossip.new("José", "Josiane est nulle").save
+        Gossip.new(params["gossip_author"], params["gossip_content"]).save
+        redirect '/'
     end
 
-    post '/gossips/new/' do
-        puts "Salut, je suis dans le serveur"
-        puts "Ceci est le contenu du hash params : #{params}"
-        puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
-        puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
-        puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
-    end
+    get '/gossips/:id/' do
+        erb :show, locals: {gossip: Gossip.find(params["id"])}
+    end    
 end
 
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -13,4 +13,29 @@ class Gossip
             csv << [@author, @content]
         end
     end
-end    
+
+    def self.all
+        all_gossips = []
+        CSV.read("./db/gossip.csv").each do |csv_line|
+            all_gossips << Gossip.new(csv_line[0], csv_line[1])
+        end
+        return all_gossips
+    end
+
+    def self.find(id)
+        CSV.open("./db/gossip.csv").each_with_index do |row, index|
+            return gossip = Gossip.new(row[0], row[1]) if index == id.to_i
+        end
+    end 
+    
+    def self.update(id, author, content)
+
+        row_array = CSV.read('db/gossip.csv')
+        row_array.each.with_index(id.to_i) do |row, index|
+            if id.to_i == index
+                row_array[index] = [author, content]
+            end    
+
+        end
+    end
+end            
